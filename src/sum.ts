c219647a5e5d7aa0090c63aa25f16b52fbcd32cd
Abhinav Jain <jainabhinav2k16@gmail.com>
2025-01-01 19:51:32 +0530

    refactor: Remove unnecessary if condition to test for single numbers

--- src/sum.ts
+++ src/sum.ts
@@ -1,6 +1,5 @@
 class SumStrings {
   add(str: String) {
-    if (!isNaN(Number(str))) return Number(str);
     const digits = str.split(",").map(Number);
 	return digits.reduce((acc, num) => acc + num, 0);
   }
